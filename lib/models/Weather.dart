25bed74f8a54658c61c5bb37fcbd48b99825c8ff
Saiko <a.m.abdelmottaleb@gmail.com>
2019-04-19 20:56:17 +0200

    - changed weather class model for better returned api data reflection

--- lib/models/Weather.dart
+++ lib/models/Weather.dart
@@ -1,16 +1,28 @@
 
+import 'dart:core';
 class Weather{
 
   static final Weather _singleton = new Weather._internal();
-
-  var weather;
-  var main;
-  String name;
-  int dt;
+  var mainWeather='';
+  double mainTemp=0, minTemp=0, maxTemp=0;
+  String name='';
+  int lastSaved=0;
 
   factory Weather(){
     return _singleton;
   }
 
   Weather._internal();
+
+  @override
+  String toString() {
+    return mainWeather.toString() + ","
+        + mainTemp.toString()+","
+        + minTemp.toString()+","
+        + maxTemp.toString()+","
+        + name+","
+        +lastSaved.toString();
+  }
+
+
 }
