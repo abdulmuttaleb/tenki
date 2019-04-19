9be967780a1550c627b32ac78e9b0fa366d35de9
Saiko <a.m.abdelmottaleb@gmail.com>
2019-04-19 20:55:36 +0200

    - changed assets map keys to reflect returned temp api call

--- lib/utils/AssetsLoader.dart
+++ lib/utils/AssetsLoader.dart
@@ -4,15 +4,23 @@ class AssetsLoader{
   Map<String, String> images = Map();
 
   AssetsLoader(){
-    images.putIfAbsent("clear sky", ()=>"assets/images/sunny.png");
-    images.putIfAbsent("few clouds", ()=>"assets/images/cloudy-sunny.png");
-    images.putIfAbsent("scattered clouds", ()=>"assets/images/cloudy.png");
-    images.putIfAbsent("broken clouds", ()=>"assets/images/cloudy.png");
-    images.putIfAbsent("shower rain", ()=>"assets/images/rainy.png");
-    images.putIfAbsent("rain", ()=>"assets/images/rainy-sun.png");
-    images.putIfAbsent("thunderstorm", ()=>"assets/images/cloudy-thunder.png");
-    images.putIfAbsent("snow", ()=>"assets/images/snowy.png");
-    images.putIfAbsent("mist", ()=>"assets/images/sunny-foggy");
+    images.putIfAbsent("Thunderstorm", ()=>"assets/images/cloudy-thunder.png");
+    images.putIfAbsent("Drizzle", ()=>"assets/images/rainy.png");
+    images.putIfAbsent("Rain", ()=>"assets/images/rainy-sun.png");
+    images.putIfAbsent("Snow", ()=>"assets/images/snowy.png");
+    images.putIfAbsent("Clear", ()=>"assets/images/sunny.png");
+    images.putIfAbsent("Clouds", ()=>"assets/images/cloudy.png");
+    images.putIfAbsent("Mist", ()=>"assets/images/sunny-foggy");
+    images.putIfAbsent("Smoke", ()=>"assets/images/sunny-foggy");
+    images.putIfAbsent("Haze", ()=>"assets/images/sunny-foggy");
+    images.putIfAbsent("Dust", ()=>"assets/images/sunny-foggy");
+    images.putIfAbsent("Fog", ()=>"assets/images/sunny-foggy");
+    images.putIfAbsent("Sand", ()=>"assets/images/sunny-foggy");
+    images.putIfAbsent("Ash", ()=>"assets/images/sunny-foggy");
+    images.putIfAbsent("Squall", ()=>"assets/images/sunny-foggy");
+    images.putIfAbsent("Tornado", ()=>"assets/images/sunny-foggy");
+    images.putIfAbsent(null, ()=>"assets/images/rainy.png");
+
   }
 
 
