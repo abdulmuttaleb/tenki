88ef6bd13e10b6bd83e563f7896e68e3eaded1a7
Saiko <a.m.abdelmottaleb@gmail.com>
2019-04-19 20:59:21 +0200

    - Data loader class - fetching api data and assigning it to weather class - saving data locally - checking if last call is older than 2 hours and making another api call if it is true to save spamming the api - loading data from local saved one if less than 2 hours

---
+++ lib/utils/DataLoader.dart
@@ -0,0 +1,93 @@
+import 'package:Tenki/models/Weather.dart';
+import "package:shared_preferences/shared_preferences.dart";
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+class DataLoader{
+
+  final _endpointUrl = "api.openweathermap.org";
+  final _apiKey = "19cb6d36e6345a5973ceac1f01c2883c";
+  var weather = new Weather();
+  Position userPosition;
+  DataLoader();
+
+  Future checkIfDataSaved() async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool isSaved = prefs.getBool("saved");
+    if(isSaved==null || !isSaved){
+      _getUserLocation();
+    }else{
+      _getSavedData().then((_) {
+        if(weather.lastSaved == null){
+          _getUserLocation();
+        }else {
+          Duration diff = DateTime.now().difference(
+              DateTime.fromMillisecondsSinceEpoch(weather.lastSaved));
+          if(diff.inHours > 2){
+            _getUserLocation();
+          }
+        }
+      });
+    }
+  }
+
+  Future _getUserLocation() async{
+    await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high).then((onValue){
+      print(onValue);
+      userPosition = onValue;
+      _fetchWeatherData();
+    });
+  }
+
+  Future _getSavedData() async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    weather.mainWeather = prefs.getString("main");
+    weather.mainTemp = prefs.getDouble("temp");
+    weather.minTemp = prefs.getDouble("temp_min");
+    weather.maxTemp = prefs.getDouble('temp_max');
+    weather.lastSaved = prefs.getInt("time_saved");
+    weather.name = prefs.getString("name");
+    print("Fetched saved data successfully!");
+  }
+
+  _saveData () async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('main', weather.mainWeather);
+    prefs.setDouble('temp', weather.mainTemp);
+    prefs.setDouble('temp_min', weather.minTemp);
+    prefs.setDouble('temp_max', weather.maxTemp);
+    prefs.setInt('time_saved', weather.lastSaved);
+    prefs.setString('name', weather.name);
+    prefs.setBool("saved", true);
+    print("saved data successfully!");
+  }
+
+  _fetchWeatherData() async{
+    var requestUri = new Uri.http(_endpointUrl,"/data/2.5/weather",{"lat":userPosition.latitude.toString(),"lon":userPosition.longitude.toString(),"appid":_apiKey});
+
+    final response = await http.get(requestUri);
+    if(response.statusCode == 200){
+      final map = json.decode(response.body);
+      print(map);
+      _assignWeatherData(map);
+      print(weather.toString());
+    }else{
+      print(response.body);
+    }
+  }
+
+  _assignWeatherData(var map){
+    final weatherMap = map["weather"];
+    final weatherMain = map["main"];
+    weatherMap.forEach((temp){
+      weather.mainWeather = temp["main"];
+    });
+    weather.mainTemp = weatherMain["temp"];
+    weather.minTemp = weatherMain["temp_min"];
+    weather.maxTemp = weatherMain["temp_max"];
+    weather.lastSaved = DateTime.now().millisecondsSinceEpoch;
+    weather.name = map['name'];
+    _saveData();
+  }
+
+}
